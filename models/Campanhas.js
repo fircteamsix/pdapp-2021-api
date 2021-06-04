edb8dcc3ee66cc3d85b5f202267cf2c20c8dd4ba
Augusto Almeida <augustalmeid@gmail.com>
2021-06-04 19:51:35 -0300

    criado modelo de campanhas

---
+++ models/Campanhas.js
@@ -0,0 +1,27 @@
+const { Model, DataTypes, BelongsTo } = require('sequelize');
+
+class Campanhas extends Model {
+    static init(sequelize){
+        super.init({
+            id_campanha: { type: DataTypes.INTEGER, primaryKey: true },
+            titulo_paciente: DataTypes.STRING,
+            data_inicio: DataTypes.STRING,
+            data_termino: DataTypes.STRING,
+            local_doacao: DataTypes.STRING,
+            tipo_sanguineo: DataTypes.STRING
+        },{
+            sequelize,
+            freezeTableName: true,
+            tableName: 'campanhas',
+            createdAt: false,
+            updatedAt: false
+        })
+    }
+    static associate(models) {
+        this.belongsTo(models.Usuario, {
+            foreignKey: 'cd_usuario',
+            as: 'usuario'
+        })
+    }
+}
+module.exports = Campanhas;
