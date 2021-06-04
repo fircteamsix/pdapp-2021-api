e5f386ac73f818328c9f4981a87045269765362f
Augusto Almeida <augustalmeid@gmail.com>
2021-06-04 19:49:39 -0300

    associação de usuario e campanha

--- database/index.js
+++ database/index.js
@@ -3,9 +3,13 @@ const dbConfig = require('../config/database');
 require('dotenv').config();
 
 const Usuario = require('../models/Usuario');
-
+const Campanhas = require('../models/Campanhas')
 const connection = new Sequelize(dbConfig);
 
 Usuario.init(connection);
+Campanhas.init(connection)
+
+Usuario.associate(connection.models)
+Campanhas.associate(connection.models)
 
 module.exports = connection;
